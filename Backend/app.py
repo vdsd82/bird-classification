f278ee178a94e472b1f2b05c2189b40c414de02e
vdsd82 <vijaydhanda@gmail.com>
2023-04-15 15:15:49 +0530

    commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,45 +1,36 @@
-import streamlit as st
-import pandas as pd
+from flask import Flask, request, jsonify
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import pandas as pd
 
-# Load the class names
-def load_classes():
-    class_df = pd.read_csv("birds.csv")
-    return class_df.set_index("class id")["labels"].to_dict()
+app = Flask(__name__)
+model = tf.keras.models.load_model('model.h5')
+class_data = pd.read_csv('birds.csv')
 
-# Load the image classification model
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model("model.h5")
-    return model
+def preprocess_image(image):
+    image = image.resize((224, 224))
+    image = np.array(image)
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
 
-# Make a prediction with the model
-def predict_image(model, img):
-    img = img.resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    predictions = model.predict(img_array)
-    class_id = np.argmax(predictions)
-    return class_id
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.files.get('image'):
+        image = Image.open(request.files['image'])
+        image = preprocess_image(image)
+        prediction = model.predict(image)
+        predicted_class = np.argmax(prediction, axis=1)[0]
+        class_info = class_data[class_data['class id'] == predicted_class].iloc[0]
+        response = {
+            'class_id': int(class_info['class id']),
+            'label': class_info['labels'],
+            'scientific_name': class_info['scientific name'],
+        }
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'No image provided'}), 400
 
-def main():
-    st.title("Bird Image Classification")
-
-    classes = load_classes()
-    model = load_model()
-
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
-        if st.button("Classify"):
-            class_id = predict_image(model, image)
-            bird_name = classes.get(class_id, "Unknown")
-            st.success(f"Classified as: {bird_name}")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
